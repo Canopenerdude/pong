d096d5cb3f325b080ffa9a296fcb9c800f7590a5
Canopenerdude <44471513+Canopenerdude@users.noreply.github.com>
2026-07-01 21:59:44 -0400

    corrected ball code and added start screen
    1st step preparation for itch launch

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -8,17 +8,10 @@ func _ready() -> void:
 	direction = 180
 	direction += randf_range(-PI / 2, PI / 2)
 	rotation = direction
-	
-	var velocity = Vector2(randf_range(800.0, 1000.0), 0.0)
+	var velocity = Vector2(randf_range(800.0, 1500), 0.0)
 	linear_velocity = velocity.rotated(direction)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	position.x = clamp(position.x, 50, screen_size.x-50)
-	position.y = clamp(position.y, 50, screen_size.y-50)
 
 
 func _on_body_entered(body):
 	if body is StaticBody2D:
-		linear_velocity *= 1.02
+		linear_velocity *= 1.08
